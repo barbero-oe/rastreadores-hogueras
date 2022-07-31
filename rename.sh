b25cb2d396b66d43bb237d0616790ed489f35d2a
Ezequiel Barbero <barbero.oe@gmail.com>
2022-07-31 20:27:53 -0300

    Renames

---
+++ rename.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mv 'src/00-introducción.html' 'src/00-introduccion.html'
+mv 'src/07-oración.html' 'src/07-oracion.html'
+mv 'src/09-perdón.html' 'src/09-perdon.html'
+mv 'src/14-crítica.html' 'src/14-critica.html'
+mv 'src/26-renovación.html' 'src/26-renovacion.html'
